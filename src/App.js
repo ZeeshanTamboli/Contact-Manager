7ae5cbb1a5358802f7d4996156c136f287f53f5f
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-10-08 21:58:50 +0530

    Components and functional components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Zeeshan';
-    const sayHello = false;
-    const showMath = true;
-    const num1 = 40;
-    const num2 = 23;
-
-    let math;
-    if (showMath) {
-      math = (
-        <h4>
-          {num1} + {num2} = {num1 + num2}
-        </h4>
-      );
-    } else {
-      math = null;
-    }
-
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        {sayHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
-        {math}
+        <Header />
+        <Contact />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: johndoe@gmail.com</li>
+          <li>Phone: 999-999-9999</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
