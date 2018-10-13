355f87e7db6b27a81a3280ffc3b81b5402bbe27b
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-10-13 15:21:30 +0530

    Added bootstrap

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
-  // static propTypes = {
-  //   name: PropTypes.string.isRequired,
-  //   email: PropTypes.string.isRequired,
-  //   phone: PropTypes.string.isRequired
-  // };
-
   render() {
     const { name, email, phone } = this.props;
     return (
-      <div>
+      <div className="card card-body mb-3">
         <h4>{name}</h4>
-        <ul>
-          <li>Email: {email}</li>
-          <li>Phone: {phone}</li>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
