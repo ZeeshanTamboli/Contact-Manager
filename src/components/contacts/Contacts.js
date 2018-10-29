e35b2c2dfc3b54e716c2ca621955916eae78290e
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-10-29 21:50:59 +0530

    React router setup

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -10,11 +10,12 @@ class Contacts extends Component {
           const { contacts } = value;
           return (
             <React.Fragment>
+              <h1 className="display-4 mb-2">
+                <span className="text-danger">Contact </span>
+                List
+              </h1>
               {contacts.map(contact => (
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                />
+                <Contact key={contact.id} contact={contact} />
               ))}
             </React.Fragment>
           );
