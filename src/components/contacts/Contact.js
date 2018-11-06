929c043a76ead391037e9a765cf673e66189a7a2
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-11-06 23:06:54 +0530

    POST & DELETE requests

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
+import axios from 'axios';
 
 class Contact extends Component {
   state = {
     showContactInfo: false
   };
 
-  onDeleteClick = (id, dispatch) => {
+  onDeleteClick = async (id, dispatch) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
     dispatch({ type: 'DELETE_CONTACT', payload: id });
   };
 
