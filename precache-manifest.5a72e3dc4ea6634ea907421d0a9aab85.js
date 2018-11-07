2340ec5276a07b1b05fe3a5fb5ebe336ad23d306
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-11-07 17:57:07 +0530

    Updates

---
+++ precache-manifest.5a72e3dc4ea6634ea907421d0a9aab85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dd574dc71532402377",
+    "url": "/Contact-Manager/static/js/runtime~main.65dd574d.js"
+  },
+  {
+    "revision": "9b3ab40ebf9797e1852f",
+    "url": "/Contact-Manager/static/js/main.9b3ab40e.chunk.js"
+  },
+  {
+    "revision": "473f992e64548d756933",
+    "url": "/Contact-Manager/static/js/1.473f992e.chunk.js"
+  },
+  {
+    "revision": "9b3ab40ebf9797e1852f",
+    "url": "/Contact-Manager/static/css/main.4fc41a1f.chunk.css"
+  },
+  {
+    "revision": "473f992e64548d756933",
+    "url": "/Contact-Manager/static/css/1.52d82160.chunk.css"
+  },
+  {
+    "revision": "4c89bb851ef29f0f10c6cb8f1290b695",
+    "url": "/Contact-Manager/index.html"
+  }
+];
