292979762dbac3c4881ece8e710b56440daddc5a
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-11-07 10:24:25 +0530

    EditContact component and fetch contact to form

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Consumer } from '../../context';
 import axios from 'axios';
 
@@ -44,6 +45,17 @@ class Contact extends Component {
                   style={{ cursor: 'pointer', float: 'right', color: 'red' }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 />
+                <Link to={`/contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: 'pointer',
+                      float: 'right',
+                      color: 'black',
+                      marginRight: '1rem'
+                    }}
+                  />
+                </Link>
               </h4>
               {showContactInfo ? (
                 <ul className="list-group">
